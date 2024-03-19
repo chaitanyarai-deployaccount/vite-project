cf003424202ab938304f1a7d8f06af7668f5bb52
chaitanyarai-deployaccount <147305573+chaitanyarai-deployaccount@users.noreply.github.com>
2024-03-19 20:57:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            autodeploy on you own
           </a>
           {' | '}
           <a
